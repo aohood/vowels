0e670de289c8934fa02feb84fda4aec71fc82f7b
Student <example@example.com>
2019-03-06 20:55:00 -0800

    add the first version of the pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,57 @@
 $("document").ready(function(){
 
-    $("#palindromes").submit(function(event){
+    $("#Pig-latin").submit(function(event){
 
       event.preventDefault();
       $("#result").show();
 
       var input= $("#txt").val();
-  
-      var n = input.localeCompare(reverseString(input));
-      if (n===0){
-        $("#result").text("Yes, "+ input+ "  is a palindrome")
-      }else{
-        $("#result").text("No, "+" "+input+" is not a palindrome")
-      }
+      var vowel =["a", "e", "i","o","u"];
+
+      if (input){
+
+        for(i=0; i<5; i++){
+          if (input.startsWith(vowel[i])){
+            $("#result").text(input+"way");
+            break;
+          }
+        }  // for the for loop
+        if(input.slice(0,2) === "qu"){
+          $("#result").text((input.slice(2,input.length))+"qu");
+        }
+        else{
+
+
+            for(i=0;i<input.length;i++){
+              var counters = new array();
+              if (input.includes(vowels(i)){
+                counter[i]= input.indexOf()
+                 var slice1= input.slice(0,i-1);
+                 alert(slice1);
+                 console.log(input);
+                 var slice2 = input.slice(i,input.length);
+                 alert(slice2);
+                 $("#result").text(slice2+slice1+"ay");
+                 break;
+              }
+
+
+            }
+
+
+
+
+         }
+             // $("#result").text(input);
+        }
+
+
+
 
 
 
  }); //fot the submit
 
-     var reverseString = function(str){
-       return str.split("").reverse().join("");
-     }
+
 });
 //for the ready
